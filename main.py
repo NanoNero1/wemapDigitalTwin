f062e2fd4795ca7cd96d81fee7a2b98632c141a0
NanoNero1 <dimitri.kachler@gmail.com>
2025-02-12 11:07:50 +0100

    test

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
 print('Hello world')
+
+import argparse
+import os
+
+
+
+def main(test):
+    print(test)
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    #parser.add_argument("square", type=int,
+    #                help="display the square of a given number")
+
+    parser.add_argument("test")
+    args = parser.parse_args()
+
+    main(args.test)
+
+"""
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("echo", help="echo the string you use here")
+args = parser.parse_args()
+print(args.echo)
+"""
+
+
+def processData():
+
+    # run process command
+
+
+
+
+
